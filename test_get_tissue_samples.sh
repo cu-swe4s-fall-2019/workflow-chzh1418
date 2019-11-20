a4ee0d21c682de035d6f43d1317b0320d5dc9c73
chzh1418 <chzh1418@colorado.edu>
2019-11-20 16:51:55 -0700

    get tissue samples scripts, functional test and unittest

---
+++ test_get_tissue_samples.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+test -e ssshtest || wget https://raw.githubusercontent.com/ryanlayer/ssshtest/master/ssshtest
+. ssshtest
+
+run test_codestyle pycodestyle test_get_tissue_samples.py
+assert_no_stdout
+
+run test_codestyle pycodestyle get_tissue_samples.py
+assert_no_stdout
+
+run test_file python get_tissue_samples.py --attribute_file GTEx_Analysis_v8_Annotations_SampleAttributesDS.txt --tissue_group 'NONE' --out_file test1.txt
+assert_exit_code 1
+rm test1.txt
+
+run test_file python get_tissue_samples.py --attribute_file GTEx_Analysis_v8_Annotations_SampleAttributesDS.txt --tissue_group 'Blood' --out_file test2.txt
+assert_exit_code 0
+rm test2.txt
